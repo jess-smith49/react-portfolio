e976dacc77bd5dc1342c586e844d861255283d19
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-23 00:11:35 -0600

    adding categories for nav bar in app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+
+//IMPORTING FILES FROM COMPONENTS 
+import Nav from './components/Nav';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+
 
 function App() {
+
+  const [sections] = useState([
+    {
+      name: 'Projects', description: 'Check out my work here!'
+    },
+    {
+      name: 'Resume', description: 'Interested in my work? Look at my background!'
+    },
+    {
+      name: 'Contact', description: 'Contact me for inquiries!'
+    }
+  ]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Nav> </Nav>
+     <main>
+       <About></About>
+     </main>
+   </div>
   );
 }
 
