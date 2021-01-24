00acfff16dbcb40e52b980158aab8e469f47c452
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-24 13:56:59 -0600

    started working on Nav component

--- portfolio/src/components/Nav/Index.js
+++ portfolio/src/components/Nav/Index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 
-
+function Nave(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
 
 export default Nav;
--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 
-
+function Projects(){
+    return(
+        <section>
+            <h1>These are a few of the Projects I have worked on during my Full Stack Web Development Course</h1>
+            
+        </section>
+    )
+}
 
 
 export default Projects;
