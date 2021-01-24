c0369333a60c05c79b932ad207ad2c28a5a09b0c
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-24 13:38:59 -0600

    contact form setup

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 
+function Contact(){
+    return(
+       <section>
+           <h1>Contact Me Below!</h1>
+           <form id = "contact-form">
+                <div>
+                    <label>What is your name?</label>
+                    <input type="text" name="name" />
+                </div>
 
+                <div>
+                    <label>Enter Your Email Address</label>
+                    <input type="email" name="email" />
+                </div>
+
+                <div>
+                    <label>Your Inquiry</label>
+                    <textarea name="message" rows="4" />
+                </div>
+                <button type="submit">Submit</button>
+           </form>
+       </section>
+    )
+}
 
 
 export default Contact;
