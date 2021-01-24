db6197fa58e99a682dec52da2eccf9c3708e5649
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-24 14:10:27 -0600

    contact form validation

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 
+
 function Contact(){
+
+    //state
+    const [formState, setFormState] =useState({name: '', email: '', message: ''})
+    const [errorMessage, setErrorMessage] = useState('')
+    const {name, email, message} = formState;
+
+    //handle submission erros
+    const submitHandler = (e) => {
+        if(!errorMessage){
+            console.log('Submit Form', formState)
+        }
+    };
+
+    //handle validations
+    const handleChanges = (e) => {
+        if(e.target.name === 'email'){
+            const isValid = validateEmail(e.target.value);
+            //if not valid
+            if(!isValid){
+                setErrorMessage('Email is not valid. Please enter a valid Email.')
+            }
+            else {
+                setErrorMessage('');
+            }
+        } else {
+            if(e.target.value.length){
+                setErrorMessage(`${e.target.name} is required`);
+            } else{
+                setErrorMessage('');
+            }
+        }
+        if(errorMessage){
+            setFormState({...formState, [e.target.name]: e.target.value});
+            console.log('Handle Form', formState);
+        }
+    }
+
+
+
     return(
        <section>
            <h1>Contact Me Below!</h1>
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 
-
+function Resume(){
+    return(
+        <section>
+            <h1>Download my Resume</h1>
+        </section>
+    )
+}
 
 
 export default Resume;
